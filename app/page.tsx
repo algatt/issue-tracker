8e3649863d0cd8096cfa3139881aa0087e1ec20b
Alan Gatt <alangatt@gmail.com>
2024-05-23 21:17:41 +0200

    Build layout of pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import Pagination from "./components/Pagination";
+
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <Pagination itemCount={100} pageSize={10} currentPage={2}></Pagination>
+  );
 }
